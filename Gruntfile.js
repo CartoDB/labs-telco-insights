fa29272919dcd729090d08035b907c0cedf030e2
piensaenpixel <piensaenpixel@gmail.com>
2016-12-13 15:05:48 +0100

    add minify

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+// Load Grunt
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    htmlmin: {                                     // Task
+      dist: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          'index.min.html': 'index.html'         // 'destination': 'source'
+        }
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'deep-insights.js/dist/deep-insights.compressed.js': ['deep-insights.js/dist/deep-insights.uncompressed.js']
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
+      }
+    }
+  });
+  // Load Grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-uncss');
+
+  // Register Grunt tasks
+  grunt.registerTask('default', ['htmlmin', 'uglify', 'cssmin']);
+};
